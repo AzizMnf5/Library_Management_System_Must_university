7a9071905244ad6807653faed89a28cf355dc26b
AzizMnf5 <mohmaedaziz.mnafakh@musteducation.tn>
2024-11-20 16:48:13 +0100

    first upload

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2024 | MUST University DataBase PFA Team : Med Aziz Mnaffakh , Hamza Belabiedh , Molka Ben Tili , Mohamed Krid , Ali chammakh 
+                </div>
+
+            </div>
+        </div>
+    </section>
